65a4c58453b46badb35b1eff47144604b9456c7d
huttanagoudrabasanagouda2-star <huttanagoudrabasanagouda2@gmail.com>
2026-09-02 14:38:44 +0530

    Update collegeinfo.py

--- collegeinfo.py
+++ collegeinfo.py
@@ -3,6 +3,6 @@ city = input("enter your college city")
 contact = input("enter your college contact number")
 location = input("enter the location of the college")
 
-print("college basic details",college+""+city)
+print("college basic details",college+" "+city)
 print("college contact is",contact)
 print("location of the college",location)
